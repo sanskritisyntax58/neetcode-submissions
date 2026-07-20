24e232af65ff057adb226844ffd88a623e0ccbb1
sanskritisyntax58 <2k24.csai1c.2410627@gmail.com>
2026-07-20 22:42:38 +0530

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<Character>[] row=new HashSet[9];
+        HashSet<Character>[] col=new HashSet[9];
+        HashSet<Character>[] box=new HashSet[9];
+        
+        for(int i=0;i<9;i++){
+        row[i]=new HashSet<>();
+        col[i]=new HashSet<>();
+        box[i]=new HashSet<>();}
+
+        for(int i=0;i<9;i++){
+        for(int j=0;j<9;j++){
+        char c=board[i][j];
+        if(c=='.')continue;
+        if(row[i].contains(c))return false;
+        if(col[j].contains(c))return false;
+        if(box[(i/3)*3+(j/3)].contains(c))return false;
+        row[i].add(c);
+        col[j].add(c);
+        box[(i/3)*3+(j/3)].add(c);
+            }
+        }
+        return true;
+    }
+}
+
