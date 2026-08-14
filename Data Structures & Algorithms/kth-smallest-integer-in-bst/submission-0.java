1e20fd3de368a12333fbface4c2f1757398a695e
sanskritisyntax58 <2k24.csai1c.2410627@gmail.com>
2026-08-14 22:49:06 +0530

    Add: kth-smallest-integer-in-bst - submission-0

---
+++ Data Structures & Algorithms/kth-smallest-integer-in-bst/submission-0.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+        int count = 0;
+            int result = 0;
+
+                public int kthSmallest(TreeNode root, int k) {
+                        inorder(root, k);
+                                return result;
+                                    }
+
+                                        private void inorder(TreeNode root, int k) {
+                                                if (root == null) {
+                                                            return;
+                                                                    }
+
+                                                                            // Left
+                                                                                    inorder(root.left, k);
+
+                                                                                            // Root
+                                                                                                    count++;
+                                                                                                            if (count == k) {
+                                                                                                                        result = root.val;
+                                                                                                                                    return;
+                                                                                                                                            }
+
+                                                                                                                                                    // Right
+                                                                                                                                                            inorder(root.right, k);
+                                                                                                                                                                }
+                                                                                                                                                                }
+
